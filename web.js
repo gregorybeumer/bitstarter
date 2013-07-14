cc713aa1439d619b7ff0dfde3786cdd56ad02842
Gregory Beumer <gregory.beumer@gmail.com>
2013-07-14 01:10:08 +0000

    Changed web.js (error)

--- web.js
+++ web.js
@@ -2,10 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
 var text = fs.readFileSync('index.html').toString();
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
